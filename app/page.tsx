072bd8d60d7133653893163bb11bc68f76281c28
Victor Massé <victor.dhamma@gmail.com>
2023-12-12 15:48:47 +0100

    up

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,19 @@ export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <Button>NextUI</Button>
-      <Button color="primary" className="rounded-md px-3 py-1.5 text-base">
-        <Icon name="add" />
-        Primary
-      </Button>
+      <div className="flex gap-1">
+        <Button>NextUI</Button>
+        <Button color="primary" className="rounded-md px-3 py-1.5 text-base">
+          Bootstrap
+        </Button>
+        <Button
+          color="primary"
+          className="gap-0 rounded-none px-3 py-1.5 text-base"
+        >
+          <Icon name="add" className="" textAround />
+          Ajouter
+        </Button>
+      </div>
     </main>
   )
 }
--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,12 +1,14 @@
 import { iconNames } from "@/scripts/out/iconNames"
 import { twMerge } from "tailwind-merge"
+import clsx from "clsx"
 
 type IconProps = {
   name: (typeof iconNames)[number]
   className?: string
+  textAround?: boolean
 }
 
-export default function Icon({ name, className }: IconProps) {
+export default function Icon({ name, className, textAround }: IconProps) {
   return (
     <span className={twMerge("material-symbols-rounded", className)}>
       {name}
